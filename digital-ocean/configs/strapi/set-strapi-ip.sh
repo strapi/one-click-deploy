0dc0d9b72530df155e0d8309cb4baa226c25188a
Derrick Mehaffy <derrickmehaffy@gmail.com>
2020-01-27 00:17:28 -0700

    Exec bootup script as strapi user

--- digital-ocean/configs/strapi/set-strapi-ip.sh
+++ digital-ocean/configs/strapi/set-strapi-ip.sh
@@ -4,8 +4,8 @@ SERVER=/srv/strapi/strapi/config/environments/development/server.json
 IP=$(curl -s ifconfig.me)
 
 if [ -f "$SERVER" ]; then
-  sed -i "s/changeme/$IP/g" "$SERVER"
-  pm2 restart strapi-develop
+  su - strapi -c "sed -i s/changeme/$IP/g $SERVER"
+  su - strapi -c "pm2 restart strapi-develop"
   sleep 10
   chown -R strapi:strapi /srv/strapi
 fi
