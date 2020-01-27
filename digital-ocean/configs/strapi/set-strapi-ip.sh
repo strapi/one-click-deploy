2ccc0a01d06ce3bc187e0e51d2511800b21194aa
Derrick Mehaffy <derrickmehaffy@gmail.com>
2020-01-27 00:07:19 -0700

    Cleanup old init and fix line endings

--- digital-ocean/configs/strapi/set-strapi-ip.sh
+++ digital-ocean/configs/strapi/set-strapi-ip.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-
 # Used to set Strapi proxy IP on first boot, you can safely delete this script
 SERVER=/srv/strapi/strapi/config/environments/development/server.json
 IP=$(curl -s ifconfig.me)
 
 if [ -f "$SERVER" ]; then
   sed -i "s/changeme/$IP/g" "$SERVER"
-  rm "$INIT"
   pm2 restart strapi-develop
   sleep 10
   chown -R strapi:strapi /srv/strapi
